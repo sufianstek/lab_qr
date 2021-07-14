627d44e3158f0585227023f070dad9194eadfbdd
chou <choukatanagari@yahoo.com>
2021-07-14 23:50:54 +0800

    sukpa

--- app.py
+++ app.py
@@ -6,10 +6,18 @@ import json
 import gspread
 import pprint
 from datetime import datetime, timedelta
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from oauth2client.service_account import ServiceAccountCredentials
 
 
 #LIVE TEST#
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+
 scopes = ["https://spreadsheets.google.com/feeds","https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
 json_creds = os.getenv("GOOGLE_SHEETS_CREDS_JSON")
 
@@ -18,69 +26,64 @@ creds_dict["private_key"] = creds_dict["private_key"].replace("\\\\n", "\n")
 creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scopes)
 client = gspread.authorize(creds)
 #LIVE TEST#
-
-
 '''
+
+
 
 #LOCAL TEST#
 scope = ["https://spreadsheets.google.com/feeds","https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
 creds = ServiceAccountCredentials.from_json_keyfile_name('ucc-pahang.json', scope)
 client = gspread.authorize(creds)
 #LOCAL TEST#
-
 '''
 
-date_times = (datetime.now() + timedelta(hours=8)).strftime("%d/%m/%Y")
 
-###SUKPA###
-spreadsheet_sukpa = client.open_by_url("https://docs.google.com/spreadsheets/d/1ILr17LgncRwNmGYmztu-QFdhp7MtOEm1Nnq2QORI3_I")
-sukpa_sh = spreadsheet_sukpa.sheet1
-
-sukpa_input = sukpa_sh.row_values(20)
-
-sukpa_input[0]= date_times
+spreadsheet = client.open_by_url("https://docs.google.com/spreadsheets/d/14vFutdJuHcszQKIpAcqyQ106AtBSebeghrRxTp9ittQ")
+sheet = spreadsheet.sheet1
 
 
-###ILKKM###
-spreadsheet_ilkkm = client.open_by_url("https://docs.google.com/spreadsheets/d/17g4wofsHYsuWBTokyY2G0ipkQsQS-eWWidvAbuN8ugM")
-ilkkm_sh = spreadsheet_ilkkm.sheet1
+class UCCBot:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        #self.bot = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+        self.bot = webdriver.Chrome()
 
-ilkkm_input = sukpa_sh.row_values(20)
-
-ilkkm_input[0]= date_times
-
-###UMP###
-spreadsheet_ump = client.open_by_url("https://docs.google.com/spreadsheets/d/1XYYkgr7laJCmynqWt9v-Inom6I9RNJPOEvENr88aNdA")
-ump_sh = spreadsheet_ump.sheet1
-
-ump_input = ump_sh.row_values(20)
-
-ump_input[0]= date_times
-
-
-###IKPKT###
-spreadsheet_ikpkt = client.open_by_url("https://docs.google.com/spreadsheets/d/1AL7AU2uyf4al1kmnyGTE2fALVf5nAMDg7k0ogAvO0Cw")
-ikpkt_sh = spreadsheet_ikpkt.sheet1
-
-ikpkt_input = ikpkt_sh.row_values(20)
-
-ikpkt_input[0]= date_times
-
+    
+    def login(self):
+        try:
+            bot = self.bot
+            bot.get('http://appshtaa.moh.gov.my/bedwatch/ucc/index.php')
+            time.sleep(5)
+            email = bot.find_element_by_name('username')
+            password = bot.find_element_by_name('password')
+            email.clear()
+            password.clear()
+            email.send_keys(self.username)
+            password.send_keys(self.password)
+            password.send_keys(Keys.RETURN)
+            time.sleep(5)
+        except:
+            bot.quit()
 
 
-###UCC STATISTICS###
-spreadsheet_uccstats = client.open_by_url("https://docs.google.com/spreadsheets/d/1qUglpzUaioqgCg_PMunjRIq4r0hu9RdcsVBhFazWhyg")
+    def scrape(self):
+        date_times = (datetime.now() + timedelta(hours=8)).strftime("%d/%m/%Y %H:%M:%S")
+        bot = self.bot
+        sukpa_available = int(bot.find_element_by_xpath("/html/body/div[2]/div/div/div/div/div/div/div/div[2]/div/div/div/div[3]").text)
+        time.sleep(5)
+        sukpa_discharges = int(bot.find_element_by_xpath("/html/body/div[2]/div/div/div/div/div/div/div/div[2]/div/div/div/div[7]").text)
+        time.sleep(5)
 
-sukpa_stats_sh = spreadsheet_uccstats.worksheet("sukpa")
-ilkkm_stats_sh = spreadsheet_uccstats.worksheet("ilkkm")
-ump_stats_sh = spreadsheet_uccstats.worksheet("ump")
-ikpkt_stats_sh = spreadsheet_uccstats.worksheet("ikpkt")
+        sheet.update('D27', sukpa_available)
+        sheet.update('G27', sukpa_discharges)
+        sheet.update('F27', date_times)
 
-sukpa_stats_sh.append_row(sukpa_input)
-ilkkm_stats_sh.append_row(ilkkm_input)
-ump_stats_sh.append_row(ump_input)
-ikpkt_stats_sh.append_row(ikpkt_input)
-print('success')
+        print("Success, SUKPA updated")
+    
 
+ucc = UCCBot('uccadmin', 'P@ssword.123')
+ucc.login()
+ucc.scrape()
 
 
